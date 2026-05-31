caa9fb69fd09c0e5a31c3e9bcfeb7fe7901ed0fb
devakapatel <stucco-good-clad@duck.com>
2026-05-31 19:23:27 +0530

    2

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
 fn main() {
-    println!("Hello, world!");
+    let endpoints = [
+        "https://solana.publicnode.com",
+        "https://solana-rpc.publicnode.com",
+    ];
+
+    let body = serde_json::json!({
+        "jsonrpc": "2.0",
+        "id": 1,
+        "method": "getSlot",
+        "params": []
+    });
+
+    for url in endpoints {
+        let resp = ureq::post(url)
+            .set("Content-Type", "application/json")
+            .send_json(&body)
+            .unwrap();
+        let val: serde_json::Value = resp.into_json().unwrap();
+        println!("{}", val["result"]);
+    }
 }
