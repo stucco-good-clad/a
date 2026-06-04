e996b3afb0fb014b517f6f2e01ba4ae4bd96e720
devakapatel <stucco-good-clad@duck.com>
2026-06-05 00:47:38 +0530

    Delete build.rs

--- build.rs
+++
@@ -1,8 +0,0 @@
-fn main() -> Result<(), Box<dyn std::error::Error>> {
-    tonic_prost_build::configure()
-        .compile_protos(
-            &["proto/old-faithful.proto", "proto/solana-storage.proto"],
-            &["proto/"],
-        )?;
-    Ok(())
-}
